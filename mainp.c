3a51e42b08385f0976dd458e37cec5f8b7089dda
IanBarreraSalas <42146304+IanBarreraSalas@users.noreply.github.com>
2018-12-05 09:53:38 -0600

    Add files via upload

--- mainp.c
+++ mainp.c
@@ -3,12 +3,13 @@
 #include <stdlib.h> 
 
 int i,j,n=10; 
-float percentx(float m0,float b0,int sigmam); 
-float percenty(float m0,float b0,int sigmam); 
+float percentx(float m0,float b0); 
+float percenty(float m0,float b0); 
 
 int main() { 
-	float m0, b0, mn, bn, px0, pxn, pxn2, ale; 
-	int delta, sigmam, sigmab;
+	float m0, b0, mn, bn; 
+	double px0, pxn=0, pxn2=0, px02, ale;
+	int delta;
 
 	FILE*resultados; 
 
@@ -17,9 +18,7 @@ int main() {
 	scanf("%f", &b0); 
 	printf("Ingrese el número deveces que quiere que el programa seejecute\n");
 	scanf("%i", &delta);
-	printf("Ingrese el error de la pendiente y el error de las ordenadas iniciales\n"); 
-	scanf("%i", &sigmam); 	   
-	scanf("%i",&sigmab); 
+
 
 	
 	
@@ -28,20 +27,24 @@ int main() {
 
 		
 		
+		
 		for(j=0;j<delta;j++){
+		
 
 			
-			mn=-40+80*(rand()/(double)RAND_MAX);
-			bn=-30+60*(rand()/(double)RAND_MAX);
+			mn=m0-(1)+(2)*(rand()/(double)RAND_MAX);
+			bn=b0-(1)+(2)*(rand()/(double)RAND_MAX);
 			//printf("%f %f\n",mn,bn);
-			px0=percentx(m0,b0,sigmam);
-			pxn=percentx(mn,bn,sigmam);
-			pxn2=percenty(mn,bn,sigmam);
-			printf("%.30f %.30f %f\n",pxn,px0,pxn2);			
+			px0=percentx(m0,b0);
+			pxn=percentx(mn,bn);
+			//px02=percenty(m0,b0);
+			pxn2=percenty(mn,bn);
+			//printf("%.30f %.30f\n",px02 ,pxn2);			
 			if(px0<pxn){
 				//printf("%.30f\n",pxn);
 			m0=mn;
 			b0=bn;
+			//fprintf(resultados,"%f %f \n", m0, b0);
 			}
 				
 
@@ -51,6 +54,7 @@ int main() {
 				if (ale<pxn2){ 
 				m0=mn; 
 				b0=bn;
+				//fprintf(resultados,"%f %f \n", m0, b0);
 				}
 			}
 
@@ -58,55 +62,66 @@ int main() {
 			
 		
 		}
-
+		
 fclose(resultados);
 return 0;
 }
-float percentx(float m0,float b0,int sigmam){
+float percentx(float m0,float b0){
 	FILE*datos; 
 	float result[10], suma=0, res2;
-	float *xobs, *yobs;
+	float *xobs, *yobs, *sigmam, *sigmab;
 	xobs=(float*)malloc(10*sizeof(float));
 	yobs=(float*)malloc(10*sizeof(float)); 
+	sigmam=(float*)malloc(10*sizeof(float));
+	sigmab=(float*)malloc(10*sizeof(float));
 	
 	datos = fopen("datos","r");
 	 
 		for (i=0;i<n;i++){ 
 
-			fscanf(datos, "%f", &xobs[i]); 
-			fscanf(datos,"%f", &yobs[i]); 
+			fscanf(datos,"%f", &xobs[i]); 
+			fscanf(datos,"%f", &yobs[i]); 	
+			fscanf(datos,"%f", &sigmam[i]); 	   
+			fscanf(datos,"%f", &sigmab[i]); 
+
 		} 
 
 	fclose(datos); 
-
 	for (i=0;i<10;i++){
 
-	result[i]= (yobs[i]-((xobs[i]*(m0))+b0)-(sigmam*sigmam));
+	result[i]= yobs[i]-(xobs[i]*(m0))+b0;
+	result[i]= pow(result[i],2)-(sigmam[i]*sigmam[i]);
 	suma+=result[i];
 	}
 	suma*=-0.5;
 	res2= pow(2.718282,suma);
 	return res2;
 }
-float percenty(float m0,float b0,int sigmam){
+float percenty(float m0,float b0){
 	FILE*datos; 
 	float result[10], suma=0, res2;
-	float *xobs, *yobs;
+	float *xobs, *yobs, *sigmam, *sigmab;
 	xobs=(float*)malloc(10*sizeof(float));
 	yobs=(float*)malloc(10*sizeof(float)); 
+	sigmam=(float*)malloc(10*sizeof(float));
+	sigmab=(float*)malloc(10*sizeof(float));
 	
 	datos = fopen("datos","r");
 	 
 		for (i=0;i<n;i++){ 
 
-			fscanf(datos, "%f", &xobs[i]); 
-			fscanf(datos,"%f", &yobs[i]); 
+			fscanf(datos,"%f", &xobs[i]); 
+			fscanf(datos,"%f", &yobs[i]); 	
+			fscanf(datos,"%f", &sigmam[i]); 	   
+			fscanf(datos,"%f", &sigmab[i]); 
+
 		} 
 
 	fclose(datos); 
 	for (i=0;i<10;i++){
 
-	result[i]= (yobs[i]-((xobs[i]*(m0))+b0)-(sigmam*sigmam));
+	result[i]= yobs[i]-(xobs[i]*(m0)+b0);
+	result[i]= pow(result[i],2)-(sigmam[i]*sigmam[i]);
 	suma+=result[i];
 	}
 	suma*=-0.5;
